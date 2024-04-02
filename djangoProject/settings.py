374ef19ad8f5b59d2c638ff3d21f4b269510fe3e
mattnobc <mattnobc@amazon.com>
2024-04-03 00:33:33 +0100

    2

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,7 +28,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
